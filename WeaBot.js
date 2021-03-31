3113d4edf0111eac2bde62cd7d4b210c86cf7fa9
Wenjing81 <xxnfdg@gmail.com>
2021-03-31 10:30:58 +0200

    Finish the card design

--- WeaBot.js
+++ WeaBot.js
@@ -1,7 +1,7 @@
-// 定义和引入 api 的键值，后面可以根据情况变化 Give api a key value, can change afterwards.
+//  Give api a key value, can change afterwards.
 const openWeatherApi ='db1f53219cee52a111e407c0e9c57ba8';
 const flickrApi = 'd083b02f7d83d378415d1feaed72229c';
-//把界面和常量对应起来. create the  connection of layout and const.
+// create the  connection of layout and const.
 const iconImg = document.getElementById('weather-icon');
 const loc = document.querySelector('#location');
 const tempC = document.querySelector('.c');
@@ -13,7 +13,7 @@ const sunsetDOM = document.querySelector('.sunset');
 
 
 window.addEventListener('load', ()=>{
-    //经度和维度，longitude and latitude.
+    //longitude and latitude.
     let long;
     let lat;
     if(navigator.geolocation){
@@ -21,16 +21,16 @@ window.addEventListener('load', ()=>{
 
             long = position.coords.longitude;
             lat = position.coords.latitude;
-            //访问api 的地址，由insomnia验证过，在console中可以得到json数据,visit api url address, tested by insomnia, can get json data in console.
+            //Visit api url address, tested by insomnia, can get json data in console.
             const apiUrl=`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${openWeatherApi}&units=metric`;
             console.log(apiUrl);
 
-            //使用fetch方法来获取json数据,获取响应。use fetch method to get json data, get the response.
+            //Use fetch method to get json data, get the response.
             fetch(apiUrl).then((response)=>{
                 return response.json();
             })
             .then((data)=>{
-                // 传送数据。 send the data to different areas in layout.
+                // Send the data to different areas in layout.
                 const{temp} = data.main;
                 const place = data.name;
                 const {description, icon} =data.weather[0];
